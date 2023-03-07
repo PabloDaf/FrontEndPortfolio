acc3257f3129075a1260ce30e668a3a6191e661d
PabloDaf <pablodafond@gmail.com>
2023-03-07 14:51:43 -0300

    mas correcciones

--- src/app/componentes/acercade/editacercade/editacercade.component.ts
+++ src/app/componentes/acercade/editacercade/editacercade.component.ts
@@ -31,6 +31,7 @@ export class EditacercadeComponent implements OnInit {
 
   onUpdate(): void{
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url
     this.personaService.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['']);
